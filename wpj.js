48ebc6f1ebaefd488e7dff35d0c885b44a088994
Ranjani Sargunaraj <ranjani789@gmail.com>
2015-07-17 15:59:53 -0400

    Javascript file
    Rudimentary interactive instructions

---
+++ wpj.js
@@ -0,0 +1,13 @@
+var firstfn= function()
+{
+var input=prompt("Are you excited about my website?!");
+if(input === "yes")
+{
+	window.alert("Yay!!!!! ^.^");
+}
+else
+{
+	window.alert("Okay...");
+}
+
+}
